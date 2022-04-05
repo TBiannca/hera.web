d1e7ae89de5bdd7aa773cbd48c9c2478cf52740a
TBiannca <bia.topircean@gmail.com>
2022-04-05 22:33:18 +0300

    [feature/7] Create login page

--- src/account/login/register-redirect.jsx
+++ src/account/login/register-redirect.jsx
@@ -5,5 +5,5 @@ import { useStyle } from '../hooks/use-style'
 
 export const RegisterRedirect = () => <Grid className={useStyle().redirect}>
   <Grid container justify={'center'} md={12} lg={12}><Typography> Nu ai un cont existent ?</Typography></Grid>
-  <Grid container justify={'center'} md={12} lg={12}><NavLink to={'/login'}> Creeaza unul. </NavLink></Grid>
+  <Grid container justify={'center'} md={12} lg={12}><NavLink to={'/'}> Creeaza unul. </NavLink></Grid>
 </Grid>
