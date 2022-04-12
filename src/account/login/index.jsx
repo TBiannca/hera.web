ec90b883568fc98573a9d0d387edfd7026562e58
TBiannca <bia.topircean@gmail.com>
2022-04-12 12:37:37 +0300

    [feature/15] Remove console warnings

--- src/account/login/index.jsx
+++ src/account/login/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Grid, Typography, useMediaQuery, useTheme } from '@material-ui/core'
-import { useStyle } from '../hooks/use-style'
+import {Grid, Typography, useMediaQuery, useTheme} from '@material-ui/core'
 import { LoginForm } from './form'
 import { RegisterRedirect } from './register-redirect'
 import { Image } from './image'
+import { useStyle } from '../hooks/use-style'
 
 export const Login = () => {
     const isDesktopResolution = useMediaQuery(useTheme().breakpoints.up('md'))
 
-    return <Grid container classes={useStyle()} justifyContent={'center'}>
+    return <Grid container className={useStyle().root} justifyContent={'center'}>
         { isDesktopResolution && <Image/> }
         <Grid item md={6} lg={6}>
             <Typography align={'center'} className={useStyle().title}>Bine ai revenit!</Typography>
--- src/account/register/index.jsx
+++ src/account/register/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Grid, Typography, useMediaQuery, useTheme } from '@material-ui/core'
-import { useStyle } from '../hooks/use-style'
 import { RegisterForm } from './form'
 import { LoginRedirect } from './login-redirect'
 import { Image } from './image'
+import { useStyle } from '../hooks/use-style'
 
 export const Register = () => {
   const isDesktopResolution = useMediaQuery(useTheme().breakpoints.up('md'))
 
-  return <Grid container classes={useStyle()} justifyContent={'center'}>
+  return <Grid container className={useStyle().root} justifyContent={'center'}>
     { isDesktopResolution && <Image/> }
     <Grid item md={6} lg={6}>
       <Typography align={'center'} className={useStyle().title}>Bine ai venit!</Typography>
