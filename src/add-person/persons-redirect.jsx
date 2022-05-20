e76500d381724caf56bb5757dcfa36de61f48d97
TBiannca <bia.topircean@gmail.com>
2022-05-20 17:37:06 +0300

    [feature/21] Change text

--- src/add-person/persons-redirect.jsx
+++ src/add-person/persons-redirect.jsx
@@ -5,5 +5,5 @@ import React from 'react'
 export const PersonsRedirect = () =><Typography style={{ marginTop: 170, letterSpacing: '0.05rem' }}>
     Apasă
     <Link to='/persoane' style={{ height: 40 }} type='submit' variant='contained'> AICI </Link>
-    pentru a vedea deja persoanele existente în sistem.
+    pentru a vedea persoanele existente în sistem.
 </Typography>
