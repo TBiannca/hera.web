2b0277261f7156f954f6dd51ef343140887c57d3
TBiannca <bia.topircean@gmail.com>
2022-06-21 17:50:42 +0300

    [feature/25] Redirect to login if user is false

--- src/add-person/index.js
+++ src/add-person/index.js
@@ -6,7 +6,7 @@ import {PersonForm} from './person-form'
 import {useStyle} from './hooks/use-style'
 import {useDispatch} from 'react-redux'
 import {PersonsRedirect} from './persons-redirect'
-import {useHistory} from 'react-router-dom'
+import {Redirect, useHistory} from 'react-router-dom'
 
 const initialValues = {
     firstName: null,
@@ -15,13 +15,14 @@ const initialValues = {
     photos: [],
 }
 
-export const AddPerson = () => {
+export const AddPerson = ({ user= true }) => {
     const dispatch = useDispatch()
     const history = useHistory()
+    const classes = useStyle()
     
-    return <Box className={useStyle().background}>
-        <Grid container className={useStyle().root}>
-            <Grid container className={useStyle().column}>
+    return user ? <Box className={classes.background}>
+        <Grid container className={classes.root}>
+            <Grid container className={classes.column}>
                 <Typography variant={'h4'} style={{marginBottom: 80, marginTop: '-16vh'}}>Adaugă o persoană în
                     sistem</Typography>
                 <Formik initialValues={initialValues} onSubmit={values => trainModel(values, dispatch, history)}>
@@ -35,5 +36,5 @@ export const AddPerson = () => {
             </Grid>
             <PersonsRedirect />
         </Grid>
-    </Box>
+    </Box> : <Redirect to={'/login'} />
 }
--- src/persons/index.jsx
+++ src/persons/index.jsx
@@ -3,15 +3,20 @@ import {Box, Grid, Typography} from '@material-ui/core'
 import {useStyle} from '../add-person/hooks/use-style'
 import {PersonsTable} from './table'
 import { Behaviour } from './behaviour'
+import {Redirect} from 'react-router-dom'
 
-export const Persons = () => <Box className={useStyle().background}>
-    <Behaviour />
-    <Grid container className={useStyle().root}>
-        <Grid container className={useStyle().column}>
-            <Typography variant={'h4'} style={{marginBottom: 80, marginTop: '-10vh'}}>
-                Persoanele existente in sistem:
-            </Typography>
-            <PersonsTable />
+export const Persons = ({user= true}) => {
+    const classes = useStyle()
+    
+    return user ? <Box className={classes.background}>
+        <Behaviour/>
+        <Grid container className={classes.root}>
+            <Grid container className={classes.column}>
+                <Typography variant={'h4'} style={{marginBottom: 80, marginTop: '-10vh'}}>
+                    Persoanele existente in sistem:
+                </Typography>
+                <PersonsTable/>
+            </Grid>
         </Grid>
-    </Grid>
-</Box>
+    </Box> : <Redirect to={'/login'} />
+}
